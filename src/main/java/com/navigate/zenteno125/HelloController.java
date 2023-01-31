726d9a6ca950f37987a95f5ef11194e60a98284b
Jose zenteno <211128@ids.upchiapas.edu.mx>
2023-01-30 23:29:36 -0600

    Structura base + Adicion de modelos(Position,Rock and Rocket) + Gravity in Rocks

--- src/main/java/com/navigate/zenteno125/HelloController.java
+++ src/main/java/com/navigate/zenteno125/HelloController.java
@@ -1,5 +1,9 @@
 package com.navigate.zenteno125;
 
+import com.navigate.zenteno125.models.Position;
+import com.navigate.zenteno125.models.Rock;
+import com.navigate.zenteno125.models.Rocket;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
@@ -7,6 +11,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 
 import java.util.Observable;
 import java.util.Observer;
@@ -15,14 +20,13 @@ public class HelloController implements Observer {
 
     @FXML
     private Button btnStartRocket;
-
     private ImageView rock;
 
-    private String path = "assets/img/entities/rocket.gif";
-
+    private Rock roca;
+    @FXML
+    private AnchorPane root;
     @FXML
     private ImageView entitieRock;
-
     @FXML
     private ImageView entitieRocket;
 
@@ -30,7 +34,7 @@ public class HelloController implements Observer {
     @FXML
     void naveOnKeyPressed(KeyEvent event){
         if (event.getCode() == KeyCode.D) {
-            if (entitieRocket.getLayoutX() > 342) {
+            if (entitieRocket.getLayoutX() > 332) {
                 entitieRocket.setLayoutX(entitieRocket.getLayoutX() );
             }else
                 entitieRocket.setLayoutX(entitieRocket.getLayoutX() + 15);
@@ -49,31 +53,41 @@ public class HelloController implements Observer {
 
     @FXML
     public void initialize(){
-
         Rocket rocket = new Rocket();
-
         Thread h1 = new Thread(rocket);
-
         h1.start();
 
 
-        rock = new ImageView(new Image(getClass().getResourceAsStream(path)));
+        rock = new ImageView(new Image(getClass().getResourceAsStream("/assets/img/entities/asteroide.png")));
+        rock.setFitWidth(50);
+        rock.setFitHeight(50);
+        rock.setLayoutX(171);
+        rock.setLayoutY(14);
+
+        root.getChildren().add(rock);
+
 
     }
 
 
     @FXML
-    void OnMouseClicketStartS(MouseEvent event) {
-
-
+    void OnMouseClicketStart(MouseEvent event) {
+        roca = new Rock();
+        roca.setPos(new Position(1, 171, 14));
+        roca.addObserver(this);
+        new Thread(roca).start();
 
     }
 
 
     @Override
     public void update(Observable o, Object arg) {
-        Rocket rocket = (Rocket) arg;
-
+        if (o instanceof Rock){
+            Position pos = (Position) arg;
+            Platform.runLater(() -> rock.setLayoutY(pos.getPosY()));
+        }else{
+            Rocket rocket = (Rocket) arg;
+        }
 
     }
 }
--- src/main/java/com/navigate/zenteno125/models/Rock.java
+++ src/main/java/com/navigate/zenteno125/models/Rock.java
@@ -8,14 +8,28 @@ import java.util.Random;
 public class Rock extends Observable implements Runnable{
 
     private Position pos;
+
+    public Position getPos() {
+        return pos;
+    }
+
+    public void setPos(Position pos) {
+        this.pos = pos;
+    }
+
     private int distanceY;
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
     private int size;
     private Random random;
     private String path = "";
-
-
-
-
     private boolean status = true;
 
     public void Rock(int size, int distanceY, String path){
@@ -26,6 +40,17 @@ public class Rock extends Observable implements Runnable{
 
     @Override
     public void run() {
+
+        while (status){
+            pos.setPosY(pos.getPosY() + 5);
+            setChanged();
+            notifyObservers(pos );
+            try {
+                Thread.sleep(50L);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
 
     }
 }
